jianghaimingc/SSVM

[Plugin] Support plugin

lib/vm/vm.cpp
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "vm/vm.h"
+#include "common/filesystem.h"
 #include "common/log.h"
 #include "host/ssvm_process/processmodule.h"
 #include "host/wasi/wasimodule.h"

luckyJ-nj/WasmEdge

Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

lib/vm/vm.cpp
@@ -2,10 +2,9 @@
 // SPDX-FileCopyrightText: 2019-2022 Second State INC
 
 #include "vm/vm.h"
-#include "vm/async.h"
-
 #include "host/wasi/wasimodule.h"
 #include "host/wasmedge_process/processmodule.h"
+#include "vm/async.h"
 
 namespace WasmEdge {
 namespace VM {